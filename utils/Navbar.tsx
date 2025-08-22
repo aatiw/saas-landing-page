00395aa0fb4a6a36484f959270304ece72027d60
atharva tiwari <aatiw69@gmail.com>
2025-08-22 23:02:43 +0530

    setup

---
+++ utils/Navbar.tsx
@@ -0,0 +1,16 @@
+const navLinks = [
+    { label: "Home", href: "#" },
+    { label: "Features", href: "#features" },
+    { label: "Integrations", href: "#integrations" },
+    { label: "FAQs", href: "#faqs" },
+];
+
+export default function Navbar() {
+    return (
+    <div className="">
+        <div className="grid grid-cols-2">
+            <div className="flex flex-row items-center justify-between"></div>
+        </div>
+    </div>
+    );
+}
