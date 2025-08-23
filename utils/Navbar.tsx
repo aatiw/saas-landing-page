4db549cc28d7617d31a748b9363501076edc4f1b
atharva tiwari <aatiw69@gmail.com>
2025-08-23 23:53:56 +0530

    navbar modification

--- utils/Navbar.tsx
+++ utils/Navbar.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { useState } from "react";
-import logo from '../assets/images/logo.svg'
+import logo from "../assets/images/logo.svg";
 
 const navLinks = [
   { label: "Home", href: "#" },
@@ -14,9 +14,13 @@ const navLinks = [
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleClick = () => {
+    setIsOpen((prev) => (!prev))
+  }
+
   return (
-    <div className="absolute top-0 w-[90vw] h-[70px] bg-transparent border-3 border-gray-500 rounded-4xl mt-3 p-4">
-      <div className="flex items-center justify-between h-full w-full">
+    <div className="absolute top-0 w-[90vw] h-auto bg-transparent border-3 border-gray-500 rounded-4xl mt-3 p-4">
+      <div className="flex items-start lg:items-center justify-between h-full w-full">
         <div className="flex flex-row items-center justify-between cursor-pointer">
           <Image src={logo} alt="logo" height={130} width={140} />
         </div>
@@ -24,7 +28,11 @@ export default function Navbar() {
         <div className="hidden lg:flex flex-row items-center justify-between gap-16">
           <div className="flex flex-row items-center justify-between space-x-6 text-white">
             {navLinks.map((link) => (
-              <a key={link.href} href={link.href} className="hover:text-[#A6E43C]">
+              <a
+                key={link.href}
+                href={link.href}
+                className="hover:text-[#A6E43C]"
+              >
                 {link.label}
               </a>
             ))}
@@ -40,41 +48,47 @@ export default function Navbar() {
           </div>
         </div>
 
-        <button className="lg:hidden text-white" onClick={() => setIsOpen(true)}>
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            viewBox="0 0 24 24"
+        <div className="lg:hidden flex flex-col items-start">
+          <button
+            className="lg:hidden text-white self-end"
+            onClick={handleClick}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
 
-        {isOpen && (
-          <div className="md:hidden mt-4 flex flex-col space-y-4">
-            {navLinks.map((link) => (
-              <a
-                key={link.href}
-                href={link.href}
-                className="text-white hover:text-green-400"
-              >
-                {link.label}
-              </a>
-            ))}
-            <button className="text-white border border-white rounded-2xl px-4 py-1">
-              Log In
-            </button>
-            <button className="text-gray-900 border border-white rounded-2xl bg-green-500 px-4 py-1">
-              Sign Up
-            </button>
-          </div>
-        )}
+          {isOpen && (
+            <div className="lg:hidden mt-4 flex flex-col items-end space-y-4">
+              {navLinks.map((link) => (
+                <a
+                  key={link.href}
+                  href={link.href}
+                  className="text-white hover:text-green-400"
+                >
+                  {link.label}
+                </a>
+              ))}
+              <button className="text-white border border-white rounded-2xl px-4 py-1 hover:bg-[#A6E43C] hover:text-[#080808]">
+                Log In
+              </button>
+              <button className="text-gray-900 border border-white rounded-2xl bg-[#A6E43C] px-4 py-1 hover:bg-transparent hover:text-white">
+                Sign Up
+              </button>
+            </div>
+          )}
+        </div>
+
       </div>
     </div>
   );
